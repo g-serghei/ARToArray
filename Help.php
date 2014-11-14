60344e478b4d44bc062327330ebd01c4abfd70c8
smart--petea <smart__petea@mail.ru>
2014-11-14 11:59:28 +0200

    moved to native 'related' logic from CActiveRecord

--- Help.php
+++ Help.php
@@ -6,7 +6,11 @@ class Help {
 			foreach($models as $model) array_push($result, self::arToArray($model));
 		} else {
 			$result = $models->attributes;	
-			foreach($models->_related as $name=>$model) array_push($result, self::arToArray($model));
+			foreach($models->relations() as $relationName=>$_) {
+				if($models->hasRelated($relationName)) {
+					array_push($result, self::arToArray($models->getRelated($relationName)));
+				}
+			}
 		}
 
 		return $result;
