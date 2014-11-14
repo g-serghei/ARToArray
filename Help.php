90d9ea1dd25e2c46402e1e3c0c8fd135b9dc775b
Serghei <sergiug@usalinksystem.net>
2014-11-14 23:10:58 +0200

    Closed #2

--- Help.php
+++ Help.php
@@ -5,6 +5,7 @@ class Help {
 			$result = array();
 			foreach($models as $model) array_push($result, self::arToArray($model));
 		} else {
+            if(!($models instanceof CActiveRecord)) throw new Exception('The passed model is not ActiveRecord type!');
 			$result = array_filter($models->attributes, function($element) { return !is_null($element);});	
 			foreach($models->relations() as $relationName=>$_) {
 				if($models->hasRelated($relationName)) {
